e762c7805f68199e5ffaa0b3190361d313521a57
yongsuk204 <yongsuk204@gmail.com>
2025-09-18 01:48:03 +0900

    Initial Commit

---
+++ RunningBuddy3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunningBuddy3
+//
+//  Created by 배용석 on 9/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunningBuddy3/RunningBuddy3App.swift
@@ -0,0 +1,17 @@
+//
+//  RunningBuddy3App.swift
+//  RunningBuddy3
+//
+//  Created by 배용석 on 9/18/25.
+//
+
+import SwiftUI
+
+@main
+struct RunningBuddy3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
